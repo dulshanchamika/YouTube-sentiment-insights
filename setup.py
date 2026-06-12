d2f8235dd4ce58ffaba7bc61b673a14cdba1dd02
Dulshan Chamika <dulshanchamika2@gmail.com>
2026-06-12 19:40:44 +0530

    Fixes

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'youtube',
-    version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'entbappy73@gmail.com',
+    version= '0.0.1',
+    author= 'Dulshan',
+    author_email= 'dulshanchamika2@gmail.com',
     packages= find_packages(),
     install_requires = []
 
